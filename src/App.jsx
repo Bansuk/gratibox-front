18cdd751855aef2d48b257bfe1bb68c4a1e7e40f
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-11-18 22:12:58 -0300

    Create home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,22 @@
-const App = () => {};
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// eslint-disable-next-line import/no-named-as-default
+import Home from './Pages/Home';
+import GlobalStyle from './Styles/styleGlobal';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Switch>
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default App;
