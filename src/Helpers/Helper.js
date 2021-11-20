9d519c37c12d1f42dbfd01ddcfb8e6792f7f3aa3
Bruno Balbuena <bbalbuena@pop-os.localdomain>
2021-11-20 03:38:46 -0300

    Adjust validation file

--- src/Helpers/Helper.js
+++
@@ -1,8 +0,0 @@
-// import { signUpUser } from '../Services/api.services';
-// import { signUpSchema } from '../Validation/Schemes';
-
-function handleSubmit(body) {
-  console.log(body);
-}
-
-export default handleSubmit();
